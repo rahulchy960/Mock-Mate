49ec6d13934f0d5141d8d34db59a30b579c792e4
rahulchy960 <rahulchy960@gmail.com>
2025-04-13 03:56:31 +0530

    route to dashboard

--- ai-interview/app/page.jsx
+++ ai-interview/app/page.jsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white flex flex-col items-center justify-center gap-4">
       <p className="text-4xl font-bold">Mock Mate</p>
       <p className="text-lg">An AI Mock Interview App</p>
-      <Button>Click Me</Button>
+      <Link href="/dashboard">
+        <Button>Go to Dashboard</Button>
+      </Link>
     </div>
   );
 }
